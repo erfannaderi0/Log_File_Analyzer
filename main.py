0416933a393b550f5e6cd7259b95903c5d5324b6
Erfan Naderi <erfannaderi0n@gmail.com>
2025-07-28 21:53:59 +0330

    log time pattern added

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import re
 
-# Compile the regex pattern once
-pattern = re.compile(r'\d{1,3}(?:\.\d{1,3}){3}')
+#this pattern is being used to find ip address
+ip_pattern = re.compile(r'\d{1,3}(?:\.\d{1,3}){3}')
+#this pattern is being used to find log time - means \[ \] => exact brakets - (.*?) => means non greedy capture of whatever is inside
+time_pattern = re.compile(r'\[(.*?)\]')
 
-def ip_finder():
+def log_parser():
     with open('sample_log.txt', 'r') as file:
         for line in file:
-            match = pattern.search(line)
-            if match:
-                print("IP Address:", match.group())
-
-ip_finder()
+            ip_match = ip_pattern.search(line)
+            time_match = time_pattern.search(line)
+            if ip_match:
+                print(f"IP: {ip_match.group()} | Time: {time_match.group(1)}")
+                
+log_parser()
